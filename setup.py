7dd7c73e85a50441dc7b9ec16d9774226fdf64d4
Phil Bull <philbull@gmail.com>
2021-06-10 16:36:10 +0100

    Try and specify a test_requires

--- setup.py
+++ setup.py
@@ -2,8 +2,4 @@
 """Setup the package."""
 from setuptools import setup
 
-tests_require = [
-    "pyuvsim @ git+git://github.com/RadioAstronomySoftwareGroup/pyuvsim",
-]
-
-setup(tests_require=tests_require)
+setup()
