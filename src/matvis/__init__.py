8ce7c3e5dcf6836d1d974cfba4e4c6f84bef4fe7
Steven Murray <murray.steveng@gmail.com>
2026-04-12 20:24:47 +0200

    maint: don't import pkg_resources any more

--- src/matvis/__init__.py
+++ src/matvis/__init__.py
@@ -1,17 +1,16 @@
 """A fast visibility simulator based on per-antenna calculations."""
 
-from pkg_resources import DistributionNotFound, get_distribution
-
+from importlib.metadata import DistributionNotFound, version
 from pathlib import Path
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
+    __version__ = version(dist_name)
 except DistributionNotFound:  # pragma: no cover
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del version, DistributionNotFound
 
 try:
     import cupy
