49f919f9111824a1baefe79691a1c75d89b72b36
Steven Murray <murray.steveng@gmail.com>
2026-04-12 20:47:08 +0200

    correct import of exception

--- src/matvis/__init__.py
+++ src/matvis/__init__.py
@@ -1,16 +1,16 @@
 """A fast visibility simulator based on per-antenna calculations."""
 
-from importlib.metadata import DistributionNotFound, version
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
     __version__ = version(dist_name)
-except DistributionNotFound:  # pragma: no cover
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
-    del version, DistributionNotFound
+    del version, PackageNotFoundError
 
 try:
     import cupy
